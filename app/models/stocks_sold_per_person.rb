27fb8dff76148f45df2365d565fc059e44656f22
Seth Fagen <sethfagen22@gmail.com>
2022-05-25 00:24:10 -0400

    Fixing bug

--- app/models/stocks_sold_per_person.rb
+++ app/models/stocks_sold_per_person.rb
@@ -9,7 +9,7 @@ class StocksSoldPerPerson < ApplicationRecord
 
   def self.find_by_user_id(user_id, date, pattern)
     compare_date = date == 5000 ? "00/00/2000 00:00:00" : DateTime.now().prev_month(date.to_i)
-    StocksSoldPerPerson.joins("INNER JOIN stocks on stock_id = stocks.id").where('user_id = ? AND stocks_sold_per_people.created_at > ? AND stocks.name like ?', user_id, compare_date, "%#{pattern}%").order(created_at: :desc)
+    StocksSoldPerPerson.joins("INNER JOIN stocks on stock_id = stocks.id").where('created_at > ? AND user_id = ? AND stocks.name like ?', user_id, compare_date, "%#{pattern}%").order(created_at: :desc)
   end
 
   def self.get_stocks_sold(user_id)
